7c5de24a13ec155019e5fbd3c8bb467f961799b9
devs1man <devikamanoharan.6@gmail.com>
2026-02-02 20:22:56 +0530

    hypertuning parameters

--- src/train.py
+++ src/train.py
@@ -2,27 +2,54 @@ from data_loader import load_data
 from model import MatrixFactorization
 import numpy as np
 import random
-import math
+
+random.seed(42)
+np.random.seed(42)
+
+def evaluate(model, val_data):
+    error = 0.0
+    for u,i,r in val_data:
+        r_hat = model.predict(u,i)
+        error += (r-r_hat)**2
+    mse = error/len(val_data)
+    rmse = np.sqrt(mse)
 
 def train():
     data, num_users, num_items = load_data("data/u.data")
-
     ratings = [r for (_, _, r) in data]
     mu = np.mean(ratings)
 
-    model = MatrixFactorization(
-        num_users = num_users,
-        num_items=num_items,
-        num_factors=20,
-        lr = 0.01,
-        reg = 0.1
-    )
-    
-    model.mu = mu
     random.shuffle(data)
     split = int(0.8 * len(data))
     train_data = data[:split]
     val_data = data[split:]
+
+    baseline_error = 0.0
+    for _,_,r in val_data:
+        baseline_error += (r-mu)**2
+    baseline_rmse = np.sqrt(baseline_error/len(val_data))
+    print(f"Baseline RMSE (global mean): {baseline_rmse:.4f}")
+
+    factors_list = [10,20,40]
+    lr_list = [0.005, 0.01]
+    reg_list = [0.05, 0.1]
+
+    results = []
+    
+    for f in factors_list:
+        for lr in lr_list:
+            for reg in reg_list:
+                print(f"\nTraining with factors={f}, lr={lr}, reg="reg)
+
+                model = MatrixFactorization(
+                    num_users = num_users,
+                    num_items=num_items,
+                    num_factors=20,
+                    lr = 0.01,
+                    reg = 0.1
+                )
+    
+                model.mu = mu
 
     epochs = 20
     for epoch in range(epochs):
@@ -33,11 +60,6 @@ def train():
 
     val_error = 0.0
     
-    baseline_error = 0.0
-    for _,_,r in val_data:
-        baseline_error =+ (r-mu)**2
-    baseline_rmse = np.sqrt(baseline_error/len(val_data))
-    print(f"Baseline RMSE (global mean): {baseline_rmse:.4f}")
 
     for u,i,r in val_data:
         rating_val = model.predict(u, i)
